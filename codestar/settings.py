75cebfe0966795e005d4fc7da96e7f36c04ad691
holly coloma <holly.life.15@gmail.com>
2024-06-16 10:30:30 +0000

    Static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # 'django-insecure-q#%u%68w-la3j-94$5)cg3%p2z1&y@9snr1!)w_fs1s30!yzsc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-hpcoloma-djangoblog-n7vf4p56xzr.ws-eu114.gitpod.io',
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Add this line to specify the directory where static files will be collected
